87f0ea86da84e326f4c1c5ef5d1a262e44be4269
Hung Nguyen <billy_hung85@yahoo.com>
2021-01-06 17:18:48 +0700

    appp.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 
 const result = dotenv.config();
 if (result.error) {
-  throw result.error;
+  console.log(result.error);
 }
 
 const app = express();
@@ -16,17 +16,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger("dev"));
 app.use("/api/", mainRouters);
 
-// mongoose
-//   .connect(process.env.DB_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("database connected");
-//   })
-//   .catch((e) => {
-//     console.log(e.message);
-//   });
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((e) => {
+    console.log(e.message);
+  });
 
 const port = process.env.PORT || 5035;
 app.get("/", (request, response) => {
