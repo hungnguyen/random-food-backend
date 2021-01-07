76fbc04eadb597391377ad6da1204a749132ef4d
Hung Nguyen <billy_hung85@yahoo.com>
2021-01-07 15:06:53 +0700

    all cors 3

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ if (result.error) {
 mainRouters.all("*", cors());
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger("dev"));
