07ab44dcacc6db5dc064e8abc1e8155e89628d6f
Hung Nguyen <billy_hung85@yahoo.com>
2021-01-07 14:10:59 +0700

    all cors

--- index.js
+++ index.js
@@ -11,28 +11,8 @@ if (result.error) {
   console.log(result.error);
 }
 
-var allowedOrigins = [
-  "https://localhost:3000",
-  "https://random-food.vercel.app",
-];
-
 const app = express();
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger("dev"));
